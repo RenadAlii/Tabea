cd1fab082aed6d02b59765035dc357ce5459c08a
Renad <ren4dali@gmail.com>
2023-07-17 13:15:05 +0300

    Create provider for UseCases data class

--- app/src/main/java/com/renad/tabea/core/di/AppModule.kt
+++ app/src/main/java/com/renad/tabea/core/di/AppModule.kt
@@ -1,6 +1,9 @@
 package com.renad.tabea.core.di
 
 import com.renad.tabea.core.util.Dispatcher
+import com.renad.tabea.domain.usecase.GetTaskUseCase
+import com.renad.tabea.domain.usecase.GetTasksUseCase
+import com.renad.tabea.domain.usecase.TaskUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,4 +15,7 @@ object AppModule {
 
     @Provides
     fun provideDispatcher(): Dispatcher = Dispatcher()
+
+    @Provides
+    fun provideTaskUseCase(getTasksUseCase: GetTasksUseCase, getTaskUseCase: GetTaskUseCase): TaskUseCases = TaskUseCases(getTaskUseCase, getTasksUseCase)
 }
